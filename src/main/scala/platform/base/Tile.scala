0f51429b493d4e64527eb4b51d8aa496fe24b80d
Zerokei <twentyeightkei@gmail.com>
2022-01-21 15:25:17 +0800

    feat: add register

--- src/main/scala/platform/base/Tile.scala
+++ src/main/scala/platform/base/Tile.scala
@@ -6,12 +6,12 @@ import ZeroCPU.wow._
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
 object GenTV extends App{
-  
   println("succeed!")
   // (new chisel3.stage.ChiselStage).execute(
   //   Array("-td", "build/verilog/base", "-X", "verilog"), 
   //   Seq(ChiselGeneratorAnnotation(() => new Total)))
 //  visualize(() => new mux_1)
-  (new chisel3.stage.ChiselStage).emitVerilog(new ALU())
-  (new chisel3.stage.ChiselStage).emitVerilog(new Mux_3())
+  //(new chisel3.stage.ChiselStage).emitVerilog(new ALU())
+  //(new chisel3.stage.ChiselStage).emitVerilog(new Mux_3())
+  (new chisel3.stage.ChiselStage).emitVerilog(new Register())
 }
